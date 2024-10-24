e5ab7eb7299165e108fdfb39e1e2738b1e17ed4e
ValhallaWebApp <valhallawebapp@gmail.com>
2024-10-24 14:14:01 +0200

    modifiche componenti home

--- alexDaTorres/src/app/modules/home/components/exhibitions/exhibitions.component.ts
+++ alexDaTorres/src/app/modules/home/components/exhibitions/exhibitions.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./exhibitions.component.scss']
 })
 export class ExhibitionsComponent {
-
+  exhibitions = [
+    {
+      title: 'Capocomino',
+      image: '../../../assets/mostre/capocomino.jpg',
+      description: 'Una descrizione di Capocomino, incluse date e dettagli dell\'evento.',
+      link: 'https://maps.google.com/?q=capocomino'
+    },
+    {
+      title: 'Isola di Tavolara',
+      image: '../../../assets/mostre/isolatavolara.jpeg',
+      description: 'Una descrizione dell\'evento sull\'isola di Tavolara.',
+      link: 'https://maps.google.com/?q=isolatavolara'
+    }
+    // Aggiungi altre esposizioni qui se necessario
+  ];
 }
